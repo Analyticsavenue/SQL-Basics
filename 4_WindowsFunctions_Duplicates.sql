7eb152c7c3c7c5753cb11c4bb64f84b34bdb488f
Analytics Avenue <134313599+Analyticsavenue@users.noreply.github.com>
2023-07-02 10:38:41 +0530

    Add files via upload

---
+++ 4_WindowsFunctions_Duplicates.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    salary DECIMAL(10, 2),
+    age INT,
+    phone_number VARCHAR(15),
+    email_id VARCHAR(255),
+    location VARCHAR(100)
+);
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (1, 50000, 30, '123-456-7890', 'john@example.com', 'London');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (2, 60000, 35, '987-654-3210', 'sarah@example.com', 'Paris');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (3, 75000, 42, '555-123-4567', 'mike@example.com', 'New York');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (4, 40000, 28, '111-222-3333', 'anna@example.com', 'Berlin');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (5, 55000, 31, '444-555-6666', 'david@example.com', 'Sydney');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (1, 65000, 36, '777-888-9999', 'laura@example.com', 'Tokyo');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (7, 70000, 39, '222-333-4444', 'peter@example.com', 'Berlin');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (8, 45000, 27, '888-999-0000', 'emily@example.com', 'London');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (9, 52000, 33, '333-444-5555', 'megan@example.com', 'New York');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (10, 58000, 37, '666-777-8888', 'alex@example.com', 'Paris');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (2, 60000, 35, '555-666-7777', 'samantha@example.com', 'London');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (12, 48000, 26, '777-888-9999', 'daniel@example.com', 'Tokyo');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (13, 62000, 32, '444-555-6666', 'lisa@example.com', 'New York');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (5, 56000, 30, '222-333-4444', 'jennifer@example.com', 'Berlin');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (15, 70000, 35, '111-222-3333', 'matthew@example.com', 'London');
+
+INSERT INTO Employee (id, salary, age, phone_number, email_id, location)
+VALUES (12, 53000, 31, '888-999-0000', 'natalie@example.com', 'Paris');
